b791d19ac60ef1e57f58e9d7aabfde35112611a6
Khailaaz <khailaazharaputri@gmail.com>
2024-10-05 12:49:35 +0700

    Add files via upload

---
+++ khaila 1_2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("panjang = 10 cm\n");
+	printf("lebar =7 cm\n");
+	
+	int P, b, L;
+	P = 10;
+	b = 7;
+	L = P * b;
+	printf ("%i", L);
+	return 0;
+}
---
+++ khaila1_1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("Nama : khaila azhara putri");
+	printf("NIM : 2441021");
+	printf("Jurusan : Teknik Informatika");
+return 0;
+}
